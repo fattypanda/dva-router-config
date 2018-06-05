b2b5b694d25a50aceda4e23d8a8ed9c664365c93
fattypanda <1076659852@qq.com>
2018-06-05 11:57:41 +0800

    删除从 react-router-config 引入的组件，方便未来改造

--- src/utils/routerConfig/renderRoutes.js
+++ src/utils/routerConfig/renderRoutes.js
@@ -1,3 +1,22 @@
-import renderRoutes from 'react-router-config/renderRoutes';
+import React from 'react';
+import Switch from 'react-router/Switch';
+import Route from 'react-router/Route';
+
+const renderRoutes = (routes, extraProps = {}, switchProps = {}) =>
+  routes ? (
+    <Switch {...switchProps}>
+      {routes.map((route, i) => (
+        <Route
+          key={route.key || i}
+          path={route.path}
+          exact={route.exact}
+          strict={route.strict}
+          render={props => (
+            <route.component {...props} {...extraProps} route={route} />
+          )}
+        />
+      ))}
+    </Switch>
+  ) : null;
 
 export default renderRoutes;
